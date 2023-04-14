c07524c4b5434c964be643de848014a5d1676346
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2023-04-14 22:09:56 +0200

    style: removed excess concat! call

--- src/honkai/consts.rs
+++ src/honkai/consts.rs
@@ -1,7 +1,7 @@
 pub static API_URI: &str = concat!("https://sdk-os-static.", "ho", "yo", "verse", ".com/bh3_global/mdk/launcher/api/resource?key=gcStgarh&launcher_id=10");
 
 /// Name of the game's data folder
-pub static DATA_FOLDER_NAME: &str = concat!("BH3_Data");
+pub static DATA_FOLDER_NAME: &str = "BH3_Data";
 
 /// List of game telemetry servers
 pub static TELEMETRY_SERVERS: &[&str] = &[
