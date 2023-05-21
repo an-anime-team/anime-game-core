355403627ca496ed2eff979a107207a9d359249b
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2023-05-21 13:21:48 +0200

    feat: increased default requests timeout to 8 seconds

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ lazy_static::lazy_static! {
     /// Default requests timeout in seconds
     pub static ref REQUESTS_TIMEOUT: u64 = match option_env!("LAUNCHER_REQUESTS_TIMEOUT") {
         Some(timeout) => timeout.parse().unwrap_or(4),
-        None => 4
+        None => 8
     };
 }
 
