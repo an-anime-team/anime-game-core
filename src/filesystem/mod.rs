091aaa9f704872768d39a873936919ff950c9f3d
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2023-08-11 23:10:50 +0200

    feat: improved `move_files` thanks to mkrsym1

--- src/filesystem/mod.rs
+++ src/filesystem/mod.rs
@@ -88,11 +88,28 @@ pub fn get_uuid(text: impl AsRef<[u8]>) -> String {
 
 /// Move files from one folder to another
 pub fn move_files(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<()> {
-    let from = from.as_ref();
-    let to = to.as_ref();
+    for entry in from.as_ref().read_dir()?.flatten() {
+        let source = entry.path();
+        let target = to.as_ref().join(entry.file_name());
 
-    for entry in from.read_dir()?.flatten() {
-        std::fs::rename(entry.path(), to.join(entry.file_name()))?;
+        if std::fs::rename(&source, &target).is_err() {
+            if source.is_dir() {
+                std::fs::create_dir_all(&target)
+                    .and_then(|_| move_files(&source, &target))
+                    .and_then(|_| std::fs::remove_dir_all(&source))?;
+            }
+
+            else if source.is_symlink() {
+                std::fs::read_link(&source)
+                    .and_then(|link_target| std::os::unix::fs::symlink(link_target, &target))
+                    .and_then(|_| std::fs::remove_file(&source))?;
+            }
+
+            else {
+                std::fs::copy(&source, &target)
+                    .and_then(|_| std::fs::remove_file(&source))?;
+            } 
+        }
     }
 
     Ok(())
