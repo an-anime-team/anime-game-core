f9685277539c440c188e38e14a205ac0112ad3c9
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2024-01-07 14:04:37 +0200

    feat: create target folder if needed in `move_files`

--- src/filesystem/mod.rs
+++ src/filesystem/mod.rs
@@ -21,14 +21,17 @@ pub fn get_uuid(text: impl AsRef<[u8]>) -> String {
 
 /// Move files from one folder to another
 pub fn move_files(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<()> {
+    if !to.as_ref().exists() {
+        std::fs::create_dir_all(to.as_ref())?;
+    }
+
     for entry in from.as_ref().read_dir()?.flatten() {
         let source = entry.path();
         let target = to.as_ref().join(entry.file_name());
 
         if std::fs::rename(&source, &target).is_err() {
             if source.is_dir() {
-                std::fs::create_dir_all(&target)
-                    .and_then(|_| move_files(&source, &target))
+                move_files(&source, &target)
                     .and_then(|_| std::fs::remove_dir_all(&source))?;
             }
 
