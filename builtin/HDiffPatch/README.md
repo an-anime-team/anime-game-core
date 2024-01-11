d2428c0962e85e526805c4c64a249e21c73b536d
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2024-01-12 00:25:39 +0200

    feat: deleted builtins

--- builtin/HDiffPatch/README.md
+++
@@ -1,4 +0,0 @@
-# HDiffPatch
-
-- Version: [4.6.6](https://github.com/sisong/HDiffPatch/releases/tag/v4.6.6)
-- License: [LICENSE](LICENSE)
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub mod network;
 pub mod filesystem;
 pub mod archive;
-pub mod builtin;
 pub mod updater;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
