f7b708e398f6c6fd4ee24d2032c62f830266689e
JohnTheCoolingFan <ivan8215145640@gmail.com>
2025-09-10 17:53:35 +0300

    Fix comment formatting in the voice package sizes table

--- src/games/star_rail/voice_data/package.rs
+++ src/games/star_rail/voice_data/package.rs
@@ -20,10 +20,9 @@ pub const VOICE_PACKAGES_SIZES: &[(&str, u64, u64, u64, u64)] = &[
     ("2.5.0", 6747272629, 7145614397, 5673877573, 5623718671),
     ("2.4.0", 6204752546, 6640826693, 5273057787, 5226442260),
     ("2.3.0", 5683360499, 6082790865, 4829957054, 4787258680),
-    ("2.2.0", 5428937683, 5790872609, 4587944120, 4548320624), /* For whatever reason, who would
-                                                                * have known, */
-    // those values are from the `size` field instead of `decompressed_size`
-    // from the game API because later one looked COMPLETELY WRONG
+    ("2.2.0", 5428937683, 5790872609, 4587944120, 4548320624), /* For whatever reason, who would have known,
+                                                                  those values are from the `size` field instead of `decompressed_size`
+                                                                  from the game API because later one looked COMPLETELY WRONG */
     ("2.1.0", 4854287546, 5198137045, 4119835425, 4066642382)
 ];
 
