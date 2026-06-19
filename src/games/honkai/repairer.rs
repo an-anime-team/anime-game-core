cd291ec60e893cd081d456bf5dc0a40befe7a2c7
JohnTheCoolingFan <ivan8215145640@gmail.com>
2026-06-19 04:12:55 +0300

    Remove non-applicable repair functions

--- src/games/honkai/repairer.rs
+++ src/games/honkai/repairer.rs
@@ -6,7 +6,6 @@ use crate::repairer::IntegrityFile;
 use crate::sophon;
 use crate::sophon::reqwest;
 use super::consts::GameEdition;
-use super::voice_data::locale::VoiceLocale;
 
 // TODO: utilize the `timeout` variable!
 
@@ -64,16 +63,6 @@ pub fn try_get_integrity_files(
     try_get_some_integrity_files(game_edition, "game", timeout)
 }
 
-/// Try to list latest voice package files.
-#[cached(result)]
-pub fn try_get_voice_integrity_files(
-    game_edition: GameEdition,
-    locale: VoiceLocale,
-    timeout: Option<u64>
-) -> anyhow::Result<Vec<IntegrityFile>> {
-    try_get_some_integrity_files(game_edition, locale.to_code(), timeout)
-}
-
 /// Try to get specific integrity file.
 ///
 /// `relative_path` must be relative to the game's root folder, so if your file
@@ -91,16 +80,6 @@ pub fn try_get_integrity_file(
         for file in files {
             if file.path == relative_path {
                 return Ok(Some(file));
-            }
-        }
-    }
-
-    for lang in VoiceLocale::list() {
-        if let Ok(files) = try_get_voice_integrity_files(game_edition, *lang, timeout) {
-            for file in files {
-                if file.path == relative_path {
-                    return Ok(Some(file));
-                }
             }
         }
     }
@@ -133,25 +112,4 @@ pub fn try_get_unused_files(
     ];
 
     crate::repairer::try_get_unused_files(game_dir, used_files, skip_names)
-}
-
-/// Try to get list of files that are not more used by the game and can be
-/// deleted.
-///
-/// ⚠️ Be aware that the game can create its own files after downloading, so
-/// "unused files" may not be really unused. It's strongly recommended to use
-/// this function only with manual control from user's side, in example to show
-/// him paths to these files and let him choose what to do with them.
-pub fn try_get_unused_voice_files(
-    game_edition: GameEdition,
-    game_dir: impl Into<PathBuf>,
-    locale: VoiceLocale,
-    timeout: Option<u64>
-) -> anyhow::Result<Vec<PathBuf>> {
-    let used_files = try_get_voice_integrity_files(game_edition, locale, timeout)?
-        .into_iter()
-        .map(|file| file.path)
-        .collect::<Vec<PathBuf>>();
-
-    crate::repairer::try_get_unused_files(game_dir, used_files, [])
 }
